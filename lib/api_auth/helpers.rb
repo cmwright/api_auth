18f4430cd750396f47181dccc571321d82b2bd65
Cody Wright <cody@vidyard.com>
2014-08-06 17:32:47 -0400

    print md5 time elapsed for api-auth

--- lib/api_auth/helpers.rb
+++ lib/api_auth/helpers.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 module ApiAuth
 
   module Helpers # :nodoc:
@@ -12,11 +14,18 @@ module ApiAuth
     end
 
     def md5_base64digest(string)
-      if Digest::MD5.respond_to?(:base64digest)
-        Digest::MD5.base64digest(string)
-      else
-        b64_encode(Digest::MD5.digest(string))
+      res = nil
+      time_taken = Benchmark.realtime do
+        if Digest::MD5.respond_to?(:base64digest)
+          res = Digest::MD5.base64digest(string)
+        else
+          res = b64_encode(Digest::MD5.digest(string))
+        end
       end
+      
+      pp "TIME TAKEN FOR MD5: ${time_taken}"
+      
+      return res
     end
 
     # Capitalizes the keys of a hash
